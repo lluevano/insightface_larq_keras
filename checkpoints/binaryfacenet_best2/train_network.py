65e2e3f8292b6f840f6f0d2fa3d9303862cff4b0
Luis Luevano <luisluevano@outlook.com>
2022-07-14 22:51:46 -0500

    add binaryfacenet saved model reference

---
+++ checkpoints/binaryfacenet_best2/train_network.py
@@ -0,0 +1,25 @@
+from tensorflow import keras
+import losses, train, models
+import tensorflow_addons as tfa
+import tensorflow as tf
+
+    # basic_model = models.buildin_models("ResNet101V2", dropout=0.4, emb_shape=512, output_layer="E")
+model_name="binaryfacenet"
+basic_model = models.buildin_models(model_name, dropout=0, emb_shape=128, output_layer="E")
+data_path = '/media/lusantlueg/WORK_CVLAB2/insightface_larq_keras/datasets/ms1m-retinaface-t1'
+eval_paths = ['/media/lusantlueg/WORK_CVLAB2/insightface_larq_keras/datasets/ms1m-retinaface-t1/lfw.bin', '/media/lusantlueg/WORK_CVLAB2/insightface_larq_keras/datasets/ms1m-retinaface-t1/cfp_fp.bin', '/media/lusantlueg/WORK_CVLAB2/insightface_larq_keras/datasets/ms1m-retinaface-t1/agedb_30.bin']
+
+tt = train.Train(data_path, save_path='keras_binaryfacenet_retina.h5', eval_paths=eval_paths,
+                basic_model=basic_model, batch_size=128, random_status=0,
+                lr_base=0.01, lr_decay=0.1, lr_decay_steps=[9,15,18], lr_min=1e-5)
+optimizer = tfa.optimizers.SGDW(learning_rate=0.1, momentum=0.9, weight_decay=5e-5)
+#optimizer = tfa.optimizers.AdamW(weight_decay=5e-5)
+sch = [
+ {"loss": losses.ArcfaceLoss(scale=16), "epoch": 20, "optimizer": optimizer},
+ #{"loss": losses.ArcfaceLoss(scale=32), "epoch": 15, "optimizer": optimizer},
+ #{"loss": losses.ArcfaceLoss(scale=64), "epoch": 15, "optimizer": optimizer},
+ # {"loss": losses.ArcfaceLoss(), "epoch": 20, "triplet": 64, "alpha": 0.35},
+]
+tt.train(sch, 0)
+
+
